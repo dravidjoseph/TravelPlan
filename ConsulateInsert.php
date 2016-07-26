e8fbbc94422b7fef7e0300b4cd5cfb20298709fb
Dravid Joseph <dravidjoseph@gmail.com>
2016-07-26 15:58:14 -0700

    Add redirect

--- ConsulateInsert.php
+++ ConsulateInsert.php
@@ -14,11 +14,13 @@ VALUES ('$city','$state','$country','$address','$zip')";
 
 if($mysqli->query($insert) === TRUE){
 	echo "New record created successfully!";
+	include 'close.php';
+	header("Location: http://people.eecs.ku.edu/~djoseph/TravelPlan/USConsulateProcessing.html")
 }
 else{
 	echo"Error: ". $insert . "<br>".$mysqli->error;
 }
 	
-include 'close.php';	
+	
 	
 ?>
