d44e28cf9f8f8fb7d1a11016958c9029ca0ecf81
Dravid Joseph <dravidjoseph@gmail.com>
2016-07-26 15:43:16 -0700

    First commit

---
+++ ConsulateInsert.php
@@ -0,0 +1,24 @@
+<?php
+	
+$city = $_POST["city"];
+$state = $_POST["state"];
+$country = $_POST["country"];
+$address = $_POST["address"];
+$zip = $_POST["zip"];
+
+#initializes database
+include 'conex.php';
+
+$insert = "INSERT INTO USConsulates (city,state,country,address,zip)
+VALUES ('$city','$state','$country','$address','$zip')";
+
+if($mysqli->query($insert) === TRUE){
+	echo "New record created successfully!";
+}
+else{
+	echo"Error: ". $insert . "<br>".$mysqli->error;
+}
+	
+include 'close.php';	
+	
+?>
---
+++ close.php
@@ -0,0 +1,9 @@
+<?php
+
+$mysqli->close();
+	
+	
+	
+	
+	
+?>
